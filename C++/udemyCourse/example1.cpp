ee193f6737dfde4de01956a10d3d0c9967259da2
ryannicholas <ryannicholas.vieira@gmail.com>
2022-12-02 10:13:18 -0300

    example1 done

---
+++ C++/udemyCourse/example1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int room_width {0};
+    int room_length {0};
+
+    cout << "Inform the room width";
+    cin >> room_width;
+
+    cout << "Inform the room length";
+    cin >> room_length;
+
+    cout << "The room area is " << room_width * room_length << endl;
+
+    return 0;
+}
