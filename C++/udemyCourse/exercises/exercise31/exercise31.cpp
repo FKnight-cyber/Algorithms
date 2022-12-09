a841ef1f15081f53a6a61fe436116abc0132358d
ryannicholas <ryannicholas.vieira@gmail.com>
2022-12-09 11:09:16 -0300

    feat: :sparkles: add exercise31

---
+++ C++/udemyCourse/exercises/exercise31/exercise31.cpp
@@ -0,0 +1,10 @@
+/*
+Add a Default Constructor to an Existing Class
+Given the Dog class in Dog.h, add a default no-args constructor that initializes the dog's 
+name to "None" and the dog's age to zero.
+
+Please add your code to the Dog.h file.
+
+You can find my solution by clicking on the solution.txt file on the left pane. But please
+make sure you give it a go yourself first, and only check the solution if you really get stuck.
+*/
