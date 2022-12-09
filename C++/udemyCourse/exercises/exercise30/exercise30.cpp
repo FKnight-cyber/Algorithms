305aa59e2349d079991cc33126d13ac1b3303246
ryannicholas <ryannicholas.vieira@gmail.com>
2022-12-09 10:28:04 -0300

    feat: :sparkles: add exercise30

---
+++ C++/udemyCourse/exercises/exercise30/exercise30.cpp
@@ -0,0 +1,14 @@
+/*
+Add more public methods to an existing class
+Given an existing Dog class in Dog.h, add the following public method to the class:
+
+get_human_years() return the dog's age * 7
+
+speak()  returns the std::string "Woof"
+
+Please add your code in the Dog.h file.
+
+You can find my solution by clicking on the solution.txt file on the left pane. 
+But please make sure you give it a go yourself first, and only check the solution 
+if you really get stuck.
+*/
