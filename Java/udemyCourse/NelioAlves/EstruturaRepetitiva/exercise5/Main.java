160ab0019273611348b74cbc939d4f66cd77bfe1
ryannicholas <ryannicholas.vieira@gmail.com>
2022-12-13 14:37:17 -0300

    feat: :sparkles: add exercise5

---
+++ Java/udemyCourse/NelioAlves/EstruturaRepetitiva/exercise5/Main.java
@@ -0,0 +1,33 @@
+/*
+Leia um valor inteiro N. Este valor será a quantidade de valores inteiros X que 
+serão lidos em seguida.
+Mostre quantos destes valores X estão dentro do intervalo [10,20] e 
+quantos estão fora do intervalo, mostrando
+essas informações conforme exemplo (use a palavra "in" para dentro do intervalo, 
+e "out" para fora do intervalo).
+ */
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int in = 0;
+        int out = 0;
+
+        System.out.print("Informe um valor: ");
+        int num = sc.nextInt();
+
+        for(int i = 1; i <= num; i++) {
+            int x = sc.nextInt();
+            if(x >= 10 && x <= 20) {
+                in++;
+            }else{
+                out++;
+            }    
+        }
+
+        System.out.printf("In: %d%n",in);
+        System.out.printf("Out: %d%n",out);
+    }
+}
