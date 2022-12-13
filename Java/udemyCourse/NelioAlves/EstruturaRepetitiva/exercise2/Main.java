6430074dd683b38ac02f02d0ba73628f91d8b6f1
ryannicholas <ryannicholas.vieira@gmail.com>
2022-12-13 12:32:27 -0300

    feat: :sparkles: add exercise2

---
+++ Java/udemyCourse/NelioAlves/EstruturaRepetitiva/exercise2/Main.java
@@ -0,0 +1,43 @@
+/*
+Escreva um programa para ler as coordenadas (X,Y) de uma quantidade indeterminada de 
+pontos no sistema
+cartesiano. Para cada ponto escrever o quadrante a que ele pertence. O algoritmo será 
+encerrado quando pelo
+menos uma de duas coordenadas for NULA (nesta situação sem escrever mensagem alguma).
+*/
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x;
+        int y;
+        boolean looping = false;
+
+        while(!looping) {
+            System.out.print("Informe X: ");
+            x = sc.nextInt();
+
+            System.out.print("Informe Y: ");
+            y = sc.nextInt();
+
+            if(x == 0 || y == 0) {
+                looping = true;
+            }
+
+            if(x > 0 && y > 0) {
+                System.out.println("primeiro");
+            }
+            if(x > 0 && y < 0) {
+                System.out.println("quarto");
+            }
+            if(x < 0 && y > 0) {
+                System.out.println("segundo");
+            }
+            if(x < 0 && y < 0) {
+                System.out.println("terceiro");
+            }
+        }
+    }
+}
