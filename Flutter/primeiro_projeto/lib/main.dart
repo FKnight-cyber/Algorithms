29133dca0639a742f0ac6d0ca9b0bd3876654f1a
ryannicholas <ryannicholas.vieira@gmail.com>
2023-01-26 19:41:01 -0300

    feat: :sparkles: add tasks difficulty

--- Flutter/primeiro_projeto/lib/main.dart
+++ Flutter/primeiro_projeto/lib/main.dart
@@ -22,11 +22,16 @@ class MyApp extends StatelessWidget {
           ),
           body: ListView(
             children: [
-              Task('Dormir', 'https://www.drronsanimalhospitalsimivalley.com/wp-content/uploads/2018/11/Dog-Sleeping-Over-Mat.jpg'),
-              Task('Flutter', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgZZOcHHzuMXdmgLHu8bUKA0Q45OjzhqBSw2oTkt85c_6xjU5fskEIDMh8B5jL6-KSQTo&usqp=CAU'),
-              Task('Java', 'https://miro.medium.com/max/625/1*C-zXmTMHObZWyZBUbdruGg.jpeg'),
-              Task('Javascript', 'https://thumbs.dreamstime.com/b/smart-working-dog-using-computer-typing-laptop-keyboard-designer-freelancer-remotely-home-pet-clothes-gray-jumper-hoodie-176711684.jpg'),
-              Task('C++', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2k6bZcHGkeHdrlBkNpxSQ3YxNbIpNdFUXPQ&usqp=CAU'),
+              Task('Dormir',
+                  'https://www.drronsanimalhospitalsimivalley.com/wp-content/uploads/2018/11/Dog-Sleeping-Over-Mat.jpg',1),
+              Task('Flutter',
+                  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgZZOcHHzuMXdmgLHu8bUKA0Q45OjzhqBSw2oTkt85c_6xjU5fskEIDMh8B5jL6-KSQTo&usqp=CAU',3),
+              Task('Java',
+                  'https://miro.medium.com/max/625/1*C-zXmTMHObZWyZBUbdruGg.jpeg',4),
+              Task('Javascript',
+                  'https://thumbs.dreamstime.com/b/smart-working-dog-using-computer-typing-laptop-keyboard-designer-freelancer-remotely-home-pet-clothes-gray-jumper-hoodie-176711684.jpg',2),
+              Task('C++',
+                  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2k6bZcHGkeHdrlBkNpxSQ3YxNbIpNdFUXPQ&usqp=CAU',5),
             ],
           ),
         ));
@@ -36,7 +41,8 @@ class MyApp extends StatelessWidget {
 class Task extends StatefulWidget {
   final String task;
   final String imageUrl;
-  const Task(this.task, this.imageUrl, {super.key});
+  final int difficulty;
+  const Task(this.task, this.imageUrl, this.difficulty, {super.key});
 
   @override
   State<Task> createState() => _TaskState();
@@ -66,12 +72,48 @@ class _TaskState extends State<Task> {
                       width: 72,
                       height: 100,
                       color: Colors.black26,
-                      child: Image.network(widget.imageUrl, fit: BoxFit.cover,),
+                      child: Image.network(
+                        widget.imageUrl,
+                        fit: BoxFit.cover,
+                      ),
                     ),
-                    Text(
-                      widget.task,
-                      style: TextStyle(
-                          fontSize: 24, overflow: TextOverflow.ellipsis),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          width: 200,
+                          child: Text(
+                            widget.task,
+                            style: TextStyle(
+                                fontSize: 24, overflow: TextOverflow.ellipsis),
+                          ),
+                        ),
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.star,
+                              color: widget.difficulty >= 1 ? Colors.blue : Colors.grey,
+                            ),
+                            Icon(
+                              Icons.star,
+                              color: widget.difficulty >= 2 ? Colors.blue : Colors.grey,
+                            ),
+                            Icon(
+                              Icons.star,
+                              color: widget.difficulty >= 3 ? Colors.blue : Colors.grey,
+                            ),
+                            Icon(
+                              Icons.star,
+                              color: widget.difficulty >= 4 ? Colors.blue : Colors.grey,
+                            ),
+                            Icon(
+                              Icons.star,
+                              color: widget.difficulty >= 5 ? Colors.blue : Colors.grey,
+                            ),
+                          ],
+                        )
+                      ],
                     ),
                     Container(
                       height: 52,
@@ -108,7 +150,7 @@ class _TaskState extends State<Task> {
                       width: 200,
                       child: LinearProgressIndicator(
                         color: Colors.white,
-                        value: nivel / 10,
+                        value: widget.difficulty > 0 ? (nivel/widget.difficulty)/10 : 1,
                       ),
                     ),
                   ),
