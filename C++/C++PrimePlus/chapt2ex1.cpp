c0fc35937d4edf5bb406bb77cc7adb6761af887e
ryannicholas <ryannicholas.vieira@gmail.com>
2023-02-07 17:07:35 -0300

    feat: :sparkles: add ex1

---
+++ C++/C++PrimePlus/chapt2ex1.cpp
@@ -0,0 +1,16 @@
+/*
+Write a C++ program that displays your name and address (or if you value your
+privacy,a fictitious name and address).
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+  cout << "My name is Jerry" << endl;
+  cout << "Street Totally real place, 367";
+
+  return 0;
+}
