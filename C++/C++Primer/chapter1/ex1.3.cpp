5edb74aeea4c70b027cd90b71baff7cd33e80617
ryannicholas <ryannicholas.vieira@gmail.com>
2023-02-08 10:43:23 -0300

    feat: :sparkles: add ex1.3

---
+++ C++/C++Primer/chapter1/ex1.3.cpp
@@ -0,0 +1,12 @@
+/*
+Exercise 1.3: Write a program to print Hello, World on the standard output.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  cout << "Hello, World" << endl;
+  return 0;
+}
