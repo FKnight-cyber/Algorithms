ddc788c38ad8cf8a5477b3a8535fdbc3a86d27f7
ryannicholas <ryannicholas.vieira@gmail.com>
2023-02-08 12:15:59 -0300

    feat: :sparkles: add exercise 1.12

---
+++ C++/C++Primer/chapter1/ex1.12.cpp
@@ -0,0 +1,13 @@
+/*
+Exercise 1.12: What does the following for loop do? What is the final value of sum?
+int sum = 0;
+for (int i = -100; i <= 100; ++i)
+sum += i;
+*/
+
+/*
+
+The loop is making a sum of the numbers in the interval [-100, 100],
+and the result of this sum is 0.
+
+*/
