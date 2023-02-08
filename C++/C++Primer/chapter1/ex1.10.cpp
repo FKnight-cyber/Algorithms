5f72512a9e91de2113a7689b8d9c3b7295619ce4
ryannicholas <ryannicholas.vieira@gmail.com>
2023-02-08 11:36:39 -0300

    feat: :sparkles: add exercise 1.10

---
+++ C++/C++Primer/chapter1/ex1.10.cpp
@@ -0,0 +1,20 @@
+/*
+Exercise 1.10: In addition to the ++ operator that adds 1 to its operand, there is a
+decrement operator (--) that subtracts 1. Use the decrement operator to write a while
+that prints the numbers from ten down to zero.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int i{10};
+
+  while(i >= 0) {
+    cout << i << endl;
+    i--;
+  }
+  
+  return 0;
+}
