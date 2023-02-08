8c38fc12a3d7a515716c7d6b4a0b51ece62c4d2d
ryannicholas <ryannicholas.vieira@gmail.com>
2023-02-08 12:23:44 -0300

    feat: :sparkles: add exercise 1.14

---
+++ C++/C++Primer/chapter1/ex1.14.cpp
@@ -0,0 +1,11 @@
+/*
+Exercise 1.14: Compare and contrast the loops that used a for with those using a
+while. Are there advantages or disadvantages to using either form?
+*/
+
+/*
+  For the given examples the while loop looked simplier, with less code to apply
+  in the given examples.
+
+  It depends, each case have an optimal way.
+*/
