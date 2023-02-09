b967a8de7147a8c3d2c2f4069ef9618a1dcb2946
ryannicholas <ryannicholas.vieira@gmail.com>
2023-02-09 18:19:52 -0300

    feat: :sparkles: add exercise 2.13

---
+++ C++/C++Primer/chapter2/ex2.13.cpp
@@ -0,0 +1,13 @@
+/*
+Exercise 2.13: What is the value of j in the following program?
+int i = 42;
+int main()
+{
+int i = 100;
+int j = i;
+}
+*/
+
+/*
+The value of j is 100, the local variable will overwrite the global value;
+*/
