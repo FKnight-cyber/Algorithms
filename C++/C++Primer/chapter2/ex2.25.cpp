dd08f5a7de1cf14447b3c2a30800421d56f4e527
Ryan <ryannicholas.vieira@gmail.com>
2023-05-01 19:44:11 -0300

    feat: :sparkles: add c++ ex 2.25

---
+++ C++/C++Primer/chapter2/ex2.25.cpp
@@ -0,0 +1,12 @@
+/*
+Exercise 2.25: Determine the types and values of each of the following variables.
+(a) int* ip, i, &r = i; (b) int i, *ip = 0; (c) int* ip, ip2;
+*/
+
+/*
+a) Variable ip is a pointer to int, i is an int, r is a reference to i.
+
+b) i is an int, ip is a pointer to 0.
+
+c) ip is a pointer to int, ip2 is an int.
+*/
